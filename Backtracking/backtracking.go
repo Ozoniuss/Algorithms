bdef8521b0a1a0c5cd0969a6e11036c6d038faab
Ozoniuss <alexbledea_2000@yahoo.com>
2022-03-26 15:44:30 +0200

    refactor(): rename files

--- Backtracking/backtracking.go
+++ Backtracking/backtracking.go
@@ -33,30 +33,53 @@ func cartezianProductIterativeOptimized(sets [][]int) [][]int {
 	return product
 }
 
-// func cartezianProductIterative(sets [][]int) [][]int {
-// 	// idea based on BFS approach
+func cartezianProductIterative(sets [][]int) [][]int {
+	// idea based on BFS approach
 
-// 	// initialize the product
-// 	product := [][]int{{}}
+	// initialize the product
+	product := [][]int{{}}
 
-// 	// for each set, add all of its elements at the end of all the already existing products
-// 	for _, set := range sets {
-// 		new_products := [][]int{}
-// 		for _, element := range set {
-// 			for _, p := range product {
-// 				new_products := append(new_products, append(p, element))
-// 				//fmt.Println(new_products)
-// 			}
-// 		}
+	// for each set, add all of its elements at the end of all the already existing products
+	for _, set := range sets {
+		new_products := [][]int{}
+		for _, element := range set {
+			for _, p := range product {
+				new_products = append(new_products, append(p, element))
+				//fmt.Println(new_products)
+			}
+		}
 
-// 	}
-// 	return product
-// }
+		// product will now point to the underlying array that new product points to
+		product = new_products
+
+	}
+	return product
+}
+
+func cartezianProductRecursive(sets [][]int) [][]int {
+
+	if len(sets) == 0 {
+		return [][]int{{}}
+	}
+	product := [][]int{}
+	first_set := sets[0]
+	other_sets := cartezianProductRecursive(sets[1:])
+	for _, element := range first_set {
+		for _, set := range other_sets {
+			product = append(product, append(set, element))
+		}
+	}
+
+	return product
+}
 
 func main() {
-	_ = cartezianProductIterativeOptimized([][]int{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}})
-	// for _, p := range prods {
-	// 	fmt.Println(p)
-	// }
-	fmt.Println("a")
+	// _ = cartezianProductIterativeOptimized([][]int{{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}})
+
+	prods := cartezianProductRecursive([][]int{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}})
+	fmt.Println(prods)
+	for _, p := range prods {
+		fmt.Println(p)
+	}
+
 }

4ee0445540df01021aeede91dcf0aaa0d7ece5a0
Ozoniuss <qscxzsewqazxcde@gmail.com>
2023-05-12 23:35:06 +0300

    leetcode(): problem 66 improvement

--- leetcode/plus-one/main.go
+++ leetcode/plus-one/main.go
@@ -2,15 +2,14 @@ package main
 
 func plusOne(digits []int) []int {
 	for i := len(digits) - 1; i >= 0; i-- {
-		if digits[i]+1 == 10 {
-			digits[i] = 0
-			if i == 0 {
-				return append([]int{1}, digits...)
-			}
-		} else {
+		if digits[i] < 9 {
 			digits[i]++
-			break
+			return digits
 		}
+		digits[i] = 0
+	}
+	if digits[0] == 0 {
+		return append([]int{1}, digits...)
 	}
 	return digits
 }

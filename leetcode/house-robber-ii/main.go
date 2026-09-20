6a02734c95d2abc97e7f89c668315f725e1fbec2
Ozoniuss <alex.bledea@cresta.ai>
2026-09-20 11:48:45 +0300

    leetcode(): problem 213

---
+++ leetcode/house-robber-ii/main.go
@@ -0,0 +1,36 @@
+package main
+
+// 10 0 1 2 2 4 3
+
+// 1 9 1 -> 9
+// 1 1 1 9 1 -> 10
+
+func robh(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if len(nums) == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	n := len(nums)
+	dp := make([]int, n)
+	dp[n-1] = nums[n-1]
+	dp[n-2] = max(nums[n-1], nums[n-2])
+	for i := n - 2; i > -1; i-- {
+		// can either skip this and just use the maximum of the next,
+		// or take it and use the maximum of the following next
+		dp[i] = max(dp[i+1], nums[i]+dp[i+2])
+	}
+	return dp[0]
+}
+
+func rob(nums []int) int {
+	v1 := robh(nums[:len(nums)-1])
+	v2 := robh(nums[1:len(nums)])
+	return max(v1, v2)
+}
+
+func main() {
+
+}
